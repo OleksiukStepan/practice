ad3e438baca68a60a5d55ece5cf08e549dd868a8
Stepan <oleksiukstepann@gmail.com>
2024-09-25 10:33:30 +0300

    rename project practice -> core

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 
 """
-URL configuration for practice project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
