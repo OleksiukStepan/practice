411c3ee72b9775ad8f27abab023a12e5166bcf6f
Stepan <oleksiukstepann@gmail.com>
2024-09-25 10:43:40 +0300

    add models Tag, Task

--- practice/models.py
+++ practice/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
+
+    def __str__(self):
+        return self.content[:50]
