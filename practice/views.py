d8e0d527fcc4e306e2b04ee717a8c99139c4e2ce
Stepan <oleksiukstepann@gmail.com>
2024-09-25 11:11:46 +0300

    update view & add index in view

--- practice/views.py
+++ practice/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from practice.models import Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(
+        request, "pages/home.html", {"tasks": tasks}
+    )
