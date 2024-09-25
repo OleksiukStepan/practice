dacf4f164a631da5b0f735fcd61a21698579642e
Stepan <oleksiukstepann@gmail.com>
2024-09-25 13:07:47 +0300

    add urls for TagList, TagCreate, TagUpdate, TagDelete

--- practice/urls.py
+++ practice/urls.py
@@ -8,6 +8,9 @@ from practice.views import (
     TaskUpdateView,
     TaskDeleteView,
     TagListView,
+    TagCreateView,
+    # TagUpdateView,
+    # TagDeleteView,
 )
 
 urlpatterns = [
@@ -16,6 +19,9 @@ urlpatterns = [
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
     path("tags/", TagListView.as_view(), name="tag_list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag_create"),
+    # path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag_update"),
+    # path("tags/<int:pk>/update/", TagDeleteView.as_view(), name="tag_delete"),
 ]
 
 app_name = "practice"
