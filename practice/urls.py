4c82d4cf27685f7722c7d4bd809a684d157ea511
Stepan <oleksiukstepann@gmail.com>
2024-09-25 11:29:07 +0300

    add task create logic

--- practice/urls.py
+++ practice/urls.py
@@ -2,10 +2,16 @@
 
 from django.urls import path
 
-from practice.views import index
+from practice.views import (
+    index,
+    TaskList,
+    # TaskCreate,
+)
 
 urlpatterns = [
     path("", index, name="home"),
+    path("tasks/", TaskList.as_view(), name="task_list"),
+    # path("tasks/create", TaskCreate.as_view(), name="task_create"),
 ]
 
 app_name = "practice"
--- practice/views.py
+++ practice/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from practice.models import Task
 
 
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by("is_done", "-created_at")
     return render(
         request, "pages/home.html", {"tasks": tasks}
     )
+
+
+class TaskList(ListView):
+    model = Task
+    template_name = "pages/task_list.html"
+    context_object_name = "tasks"
+    ordering = ["is_done", "-created_at"]
+
